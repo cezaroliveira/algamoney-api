ee63e50b86d1d37b70f15ece061b1fb0634b371f
cezaroliveira <cezaroliveira1987@gmail.com>
2020-07-24 18:25:48 -0300

    Inclusão do método que cadastra uma Categoria.

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,18 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
@@ -20,6 +27,22 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+		// Cria uma URI para o objeto que foi salvo
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+
+		// Retorna como localização a URI para acessar o objeto que foi salvo
+		response.setHeader("Location", uri.toASCIIString());
+
+		// Cria uma resposta com o código 201 e retorna o JSON do objeto que foi salvo no banco
+		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 
 }
