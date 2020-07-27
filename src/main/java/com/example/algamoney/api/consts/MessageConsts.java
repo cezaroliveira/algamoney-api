f711cdbd4e06c213b9edf5ffb1c9cccf332990ef
cezaroliveira <cezaroliveira1987@gmail.com>
2020-07-27 10:43:42 -0300

    Inclusão do tratamento de exceções para mensagem amigáveis ao usuário.

---
+++ src/main/java/com/example/algamoney/api/consts/MessageConsts.java
@@ -0,0 +1,9 @@
+package com.example.algamoney.api.consts;
+
+public final class MessageConsts {
+
+	private MessageConsts() {}
+
+	public static final String MENSAGEM_INVALIDA = "mensagem.invalida";
+
+}
