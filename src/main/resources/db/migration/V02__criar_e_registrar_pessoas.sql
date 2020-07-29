03c02a9846aa61276ab6931f2e30652328305fdf
cezaroliveira <cezaroliveira1987@gmail.com>
2020-07-29 08:50:54 -0300

    Retorno do conteúdo do script da entidade pessoa que tinha sido removido indevidamente.

---
+++ src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
@@ -0,0 +1,18 @@
+CREATE TABLE pessoa (
+	codigo BIGINT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL,
+	logradouro VARCHAR(50) NULL,
+	numero VARCHAR(50) NULL,
+	complemento VARCHAR(50) NULL,
+	bairro VARCHAR(30) NULL,
+	cep VARCHAR(8) NULL,
+	cidade VARCHAR(30) NULL,
+	estado VARCHAR(2) NULL,
+	ativo BOOLEAN NOT NULL DEFAULT TRUE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado) VALUES ('Pedro', 'Rua do Pedro', '4', null, 'Nova Esperança', '34789564', 'Belo Horizonte', 'MG');
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado) VALUES ('Flávia', 'Rua da Flávia', '789', 'Bloco 3, Apto 504', 'Terezinha', '32341260', 'São José', 'SP');
+INSERT INTO pessoa (nome) VALUES ('José');
+INSERT INTO pessoa (nome) VALUES ('João');
+INSERT INTO pessoa (nome) VALUES ('Maria');
