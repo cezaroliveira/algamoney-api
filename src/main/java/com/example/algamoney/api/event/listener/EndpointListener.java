0c9eba69ca9a05a310cd1995fa6de984235625c8
cezaroliveira <cezaroliveira1987@gmail.com>
2020-08-03 13:38:03 -0300

    Inclusão do listener que lista os endpoints no log durante a inicialização.

---
+++ src/main/java/com/example/algamoney/api/event/listener/EndpointListener.java
@@ -0,0 +1,24 @@
+package com.example.algamoney.api.event.listener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@Component
+public class EndpointListener implements ApplicationListener<ApplicationEvent> {
+
+	@Autowired
+	private RequestMappingHandlerMapping requestHandlerMapping;
+
+	@Override
+	public void onApplicationEvent(ApplicationEvent event) {
+		if (event instanceof ContextRefreshedEvent) {
+			requestHandlerMapping.getHandlerMethods()
+			.forEach((key, value) ->  System.out.println(key + " - " + value));
+		}
+	}
+
+}
