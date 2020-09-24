9031a5068382694f7333543794ac17d820c5c025
cezaroliveira <cezaroliveira1987@gmail.com>
2020-09-24 20:23:13 -0300

    Migrating MySQL local to Docker.

---
+++ src/main/resources/db/1-docker-build-mysql.cmd
@@ -0,0 +1,2 @@
+@REM Constroi uma imagem a partir de um arquivo DockerFile
+docker build -t mysql-image -f src\main\resources\db\DockerFile .
---
+++ src/main/resources/db/2-docker-run-with-volume-mysql.cmd
@@ -0,0 +1,4 @@
+@REM Executa uma imagem no docker montando um volume
+@REM Erro: Client does not support authentication protocol requested by server; consider upgrading MySQL client
+@REM Solução: Passar o argumento --default-authentication-plugin=mysql_native_password para o MySQL
+docker run -d -v %cd%/src/main/resources/db/data:/var/lib/mysql -p 3306:3306 --rm --name mysql-container mysql-image --default-authentication-plugin=mysql_native_password
