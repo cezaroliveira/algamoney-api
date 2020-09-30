75a732a47fbdba490295e3a5134985b4dc2bdba8
cezaroliveira <cezaroliveira1987@gmail.com>
2020-09-30 20:58:35 -0300

    Correcting Maria password.

--- src/main/java/com/example/algamoney/api/security/PasswordGenerator.java
+++ src/main/java/com/example/algamoney/api/security/PasswordGenerator.java
@@ -6,7 +6,7 @@ public final class PasswordGenerator {
 
 	public static void main(String[] args) {
 		System.out.println(new BCryptPasswordEncoder().encode("admin"));
-		System.out.println(new BCryptPasswordEncoder().encode("marina"));
+		System.out.println(new BCryptPasswordEncoder().encode("maria"));
 	}
 	
 }
