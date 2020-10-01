587cde41be24719f9dbf2162d5c0b51b32e4c55f
cezaroliveira <cezaroliveira1987@gmail.com>
2020-09-30 21:04:23 -0300

    Changing to use permission from user in database to access resources.

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,6 +1,15 @@
 package com.example.algamoney.api.resource;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.model.Categoria;
@@ -10,5 +19,20 @@ import com.example.algamoney.api.service.CategoriaService;
 @RestController
 @RequestMapping(path = "/categorias")
 public class CategoriaResource extends AbstractCrudResource<Categoria, CategoriaRepository, CategoriaService> {
+
+	@Override
+	@PreAuthorize(value = "hasRole('ROLE_PESQUISAR_CATEGORIA')")
+	public ResponseEntity<Page<Categoria>> filtrar(
+			@RequestParam(value = "$search", required = false) String search,
+			@RequestParam(value = "$filter", required = false) String filter,
+			Pageable pageable) {
+		return super.filtrar(search, filter, pageable);
+	}
+
+	@Override
+	@PreAuthorize(value = "hasRole('ROLE_CADASTRAR_CATEGORIA')")
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria entity, HttpServletResponse response) {
+		return super.criar(entity, response);
+	}
 
 }
